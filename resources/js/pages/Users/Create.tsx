3d4c488bfc67f89c85673095b82363d3d918c1bf
MrSteveW <MrSteveW@users.noreply.github.com>
2026-02-03 19:21:41 +0000

    feat: user create role dropdown menu

--- resources/js/pages/Users/Create.tsx
+++ resources/js/pages/Users/Create.tsx
@@ -1,17 +1,29 @@
 import { Form, Head } from '@inertiajs/react';
+import { useState } from 'react';
 import InputError from '@/components/auth/input-error';
 import { Button } from '@/components/ui/button';
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuRadioGroup,
+    DropdownMenuRadioItem,
+    DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Spinner } from '@/components/ui/spinner';
 import AppLayout from '@/layouts/app-layout';
 
-export default function Create() {
+type CreateProps = {
+    roles: string[];
+};
+
+export default function Create({ roles }: CreateProps) {
+    const [selectedRole, setSelectedRole] = useState<string>('');
     return (
         <AppLayout>
             <Head title="Users" />
             <div className="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4">
-                {/* WAYFINDER?  {...store.form()} */}
                 <Form
                     action="/users"
                     method="post"
@@ -60,12 +72,34 @@ export default function Create() {
 
                                 <div className="grid gap-2">
                                     <Label htmlFor="role">Role</Label>
-                                    <Input
-                                        id="role"
-                                        type="text"
+                                    <DropdownMenu>
+                                        <DropdownMenuTrigger asChild>
+                                            <Button variant="outline">
+                                                {selectedRole ||
+                                                    'Select access'}
+                                            </Button>
+                                        </DropdownMenuTrigger>
+
+                                        <DropdownMenuContent>
+                                            <DropdownMenuRadioGroup
+                                                value={selectedRole}
+                                                onValueChange={setSelectedRole}
+                                            >
+                                                {roles.map((role) => (
+                                                    <DropdownMenuRadioItem
+                                                        key={role}
+                                                        value={role}
+                                                    >
+                                                        {role}
+                                                    </DropdownMenuRadioItem>
+                                                ))}
+                                            </DropdownMenuRadioGroup>
+                                        </DropdownMenuContent>
+                                    </DropdownMenu>
+                                    <input
+                                        type="hidden"
                                         name="role"
-                                        required
-                                        autoComplete="off"
+                                        value={selectedRole}
                                     />
                                     <InputError message={errors.role} />
                                 </div>
